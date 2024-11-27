d426d7f70239ebec50fbeac03d1dc2cdb9962eaa
abedin-ansari <jainulansari579@gmail.com>
2024-11-27 14:15:47 +0530

    Updated code

--- 18_LinkedList P-2/DoublyLl.java
+++ 18_LinkedList P-2/DoublyLl.java
@@ -1,108 +1,142 @@
-public class DoublyLl {
-    public class Node {
-        int data;
-        Node next;
-        Node prev;
+/**
+ * A Doubly Linked List (DLL) is a type of linked list in which each node contains three parts:
+ * 1. Data: The value stored in the node.
+ * 2. Next Pointer: A reference to the next node in the list.
+ * 3. Previous Pointer: A reference to the previous node in the list.
+ * 
+ * In a Doubly Linked List, traversal is possible in both forward and backward directions,
+ * making it more flexible than a singly linked list.
+ */
 
+ public class DoublyLl {
+
+    // Definition of a Node in a Doubly Linked List
+    public class Node {
+        int data; // Data stored in the node
+        Node next; // Pointer to the next node
+        Node prev; // Pointer to the previous node
+
+        // Constructor to initialize a new node
         public Node(int data){
             this.data = data;
             this.next = null;
             this.prev = null;
         }
     }
+
+    // Head points to the first node, tail to the last node in the list
     public static Node head;
     public static Node tail;
-    public static int size;
+    public static int size; // Tracks the size of the doubly linked list
 
-    // Add
+    /**
+     * Adds a new node with the specified data at the beginning of the list.
+     * Doubly Linked List ensures each node links to the next and previous nodes.
+     */
     public void addFirst(int data){
-        Node newNode = new Node(data);
-        size++;
-        if(head == null){
-            head = tail = newNode;
+        Node newNode = new Node(data); // Create a new node
+        size++; // Increment the size
+        if(head == null){ // If the list is empty
+            head = tail = newNode; // Both head and tail point to the new node
             return;
         }
 
+        // Link the new node to the current head and update the head pointer
         newNode.next = head;
         head.prev = newNode;
         head = newNode;
     }
-    // Add Last
+
+    /**
+     * Adds a new node with the specified data at the end of the list.
+     */
     public void addLast(int data){
-        Node newNode = new Node(data);
-        size++;
-        if(head == null){
-            tail = head = newNode;
+        Node newNode = new Node(data); // Create a new node
+        size++; // Increment the size
+        if(head == null){ // If the list is empty
+            head = tail = newNode; // Both head and tail point to the new node
             return;
         }
 
+        // Link the new node to the current tail and update the tail pointer
         newNode.prev = tail;
         tail.next = newNode;
         tail = newNode;
-        
     }
 
-    // Print
+    /**
+     * Prints all the elements of the doubly linked list.
+     * Each node's data is printed along with its link to the next node.
+     */
     public void print(){
         Node temp = head;
         while(temp != null){
-            System.out.print(temp.data + "<->");
-            temp = temp.next;
+            System.out.print(temp.data + "<->"); // Print data of each node
+            temp = temp.next; // Move to the next node
         }
-        System.out.println("null");
+        System.out.println("null"); // End of the list
     }
 
-    // Remove
+    /**
+     * Removes and returns the first node of the list.
+     */
     public int removeFirst(){
-        if(head == null){
+        if(head == null){ // If the list is empty
             System.out.println("Ll is empty");
             return Integer.MIN_VALUE;
         }
-        if(size == 1){
-            int val = head.data;
-            head = tail = null;
+        if(size == 1){ // If there is only one element
+            int val = head.data; // Store the data
+            head = tail = null; // Empty the list
             size--;
             return val;
         }
-        int val = head.data;
-        head = head.next;
-        head.prev = null;
+        int val = head.data; // Store the data to return
+        head = head.next; // Move head to the next node
+        head.prev = null; // Remove link to the old head
         size--;
         return val;
     }
-    // Remove Last
+
+    /**
+     * Removes and returns the last node of the list.
+     */
     public int removeLast(){
-        if(head == null){
+        if(head == null){ // If the list is empty
             System.out.println("LL is Empty");
             return Integer.MIN_VALUE;
         }
-        if(size == 1){
-            int val = tail.data;
-            head = tail = null;
+        if(size == 1){ // If there is only one element
+            int val = tail.data; // Store the data
+            head = tail = null; // Empty the list
             size--;
             return val;
         }
-        int val = tail.data;
-        tail = tail.prev;
-        tail.next = null;
+        int val = tail.data; // Store the data to return
+        tail = tail.prev; // Move tail to the previous node
+        tail.next = null; // Remove link to the old tail
         size--;
         return val;
     }
 
+    /**
+     * Reverses the doubly linked list.
+     * Swaps the next and previous pointers of all nodes to reverse their direction.
+     */
     public void reverse(){
         Node curr = head;
-        Node prev = null;
+        Node prev = null; // Initialize previous node as null
         Node next;
 
         while (curr != null) {
-            next = curr.next;
-            curr.next = prev;
-            curr.prev = next;
+            next = curr.next; // Store reference to the next node
+            curr.next = prev; // Reverse the next pointer
+            curr.prev = next; // Reverse the prev pointer
 
-            prev = curr;
-            curr = next;
+            prev = curr; // Move prev to current
+            curr = next; // Move current to next
         }
-        head = prev;
+        head = prev; // Update head to the last node
     }
 
     public static void main(String[] args) {
