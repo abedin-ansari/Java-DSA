15a8041809da8724bd0eb6b492422b03d741f6d6
abedin-ansari <jainulansari579@gmail.com>
2024-12-29 19:33:13 +0530

    Reverse Queue Question

--- 20_Queues/InterLeave.java
+++ 20_Queues/InterLeave.java
@@ -4,7 +4,14 @@ import java.util.Queue;
 public class InterLeave {
     public static void interLeave(Queue<Integer> q){
         Queue<Integer> firstHalf = new LinkedList<>();
-        
+        int size = q.size();
+        for(int i=0; i<size/2; i++){
+            firstHalf.add(q.remove());
+        }
+        while(!firstHalf.isEmpty()){
+            q.add(firstHalf.remove());
+            q.add(q.remove());
+        }
     }
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>();
@@ -20,5 +27,9 @@ public class InterLeave {
         q.add(10);
 
         interLeave(q);
+        while(!q.isEmpty()){
+            System.out.print(q.remove() + " ");
+        }
+        System.out.println();
     }
 }
