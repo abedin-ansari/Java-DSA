b4560f8d32a42ba3f89894c89edaabbca921aa8e
abedin-ansari <jainulansari579@gmail.com>
2025-08-07 13:38:22 +0530

    updated code

--- 3_Operators/Assignment.java
+++ 3_Operators/Assignment.java
@@ -2,9 +2,9 @@ public class Assignment {
     public static void main(String[] args){
         int x, y, z;
         x = y = z = 2;
-        x += y;  // (x = x + y)
-        y -= z;  // (y = y - z)
-        z /= (x + y);
+        x += y;  // (x = x + y) =4
+        y -= z;  // (y = y - z) =0
+        z /= (x + y); // 2/4
         System.out.println(x + " " + y + " " + z);
     }
 }
--- 3_Operators/Logical.java
+++ 3_Operators/Logical.java
@@ -7,7 +7,7 @@ public class Logical {
         if(z > y || z < x){  // ( || -> if one side is true then it will return true it is not neseccery both true for logical or ||)
         System.out.println("Java");
         }
-        if((y+200) < x ! (y+150) < z){ // ( ! -> logical not is (true if exp is false and vica verse)).
+        if((y+200) < x != (y+150) < z){ // ( ! -> logical not is (true if exp is false and vica verse)).
         System.out.println("Hello Java");
         }
     }
